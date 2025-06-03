1a4ac7e2fa293834a7d8767318c2a235993120dd
Cedric Okai <okaicedric4@gmail.com>
2025-06-03 14:28:02 +0000

    modified

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Cedricportfolio.github.io/',
+  base: '/',
   plugins: [react()],
   css: {
     postcss: {
