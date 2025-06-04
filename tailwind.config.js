8b3b93417a5f7c72ed64100421987216acc24f41
Cedric Okai <okaicedric4@gmail.com>
2025-06-04 10:36:38 +0000

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 export const content = [
   './index.html',
   './src/**/*.{js,ts,jsx,tsx}',
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Cedricportfolio',
   plugins: [react()],
   css: {
     postcss: {
